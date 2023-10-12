6ee8ad5dd1d2d53b21745eefddbaf8597c70740e
Anthea Ip <antheaip964@gmail.com>
2023-10-12 13:07:53 -0400

    Saving; InvestmentServiceTest passes

--- src/test/java/com/example/stock_port/InvestmentServiceTest.java
+++ src/test/java/com/example/stock_port/InvestmentServiceTest.java
@@ -1,4 +1,34 @@
 package com.example.stock_port;
 
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+
+import java.math.BigDecimal;
+
+import static org.mockito.Mockito.*;
+
+@SpringBootTest
 public class InvestmentServiceTest {
+    @MockBean
+    private InvestmentRepository mockInvestmentRepository;
+
+    @Test
+    public void testAddInvestment() {
+        System.out.println("Running the service test");
+        InvestmentService investmentService = new InvestmentService(mockInvestmentRepository);
+        System.out.println("created the new mockInvestRepo");
+        Investment investment = new Investment("Example", new BigDecimal("1000.0"));
+        System.out.println("created new investment");
+
+        // Add operation
+        investmentService.addInvestment(investment);
+        System.out.println("added investment");
+        // Verify that save method of the repo was called with the investment ob
+        verify(mockInvestmentRepository, times(1)).save(investment);
+        System.out.println("verified investment");
+
+    }
+
+
 }
